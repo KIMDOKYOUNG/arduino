59af5e0adc511a9273a6f3c3162962445ea8f335
KIMDOKYOUNG <42798576+KIMDOKYOUNG@users.noreply.github.com>
2020-07-06 16:54:59 +0900

    Add files via upload

---
+++ ex1.ino
@@ -0,0 +1,14 @@
+int led = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led,HIGH); //1
+  delay(1000);
+  digitalWrite(led,LOW); //0
+  delay(1000);
+}
