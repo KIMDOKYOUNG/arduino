ef757b174e310352efdf4998b4c6024915d61813
KIMDOKYOUNG <42798576+KIMDOKYOUNG@users.noreply.github.com>
2020-07-07 16:05:20 +0900

    Add files via upload

---
+++ botten_LED_2_delay.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(8,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value = digitalRead(8);// 8핀으로 입력받는 값을 저장한다.
+  if(value == HIGH){
+    digitalWrite(13,HIGH);// 8핀이 눌리면 5V 전기가 들어온다.
+    digitalWrite(12,HIGH);
+    delay(3000);
+  } else{
+    digitalWrite(13,LOW);
+    digitalWrite(12,LOW);
+  }
+}
---
+++ ex3.ino
@@ -0,0 +1,19 @@
+// 반딧불이 led 만들기 analog 신호여서 pinmode 안 만들어 줘도 된다.
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = 0; i <256;i++){
+    analogWrite(9,i);
+    analogWrite(10,i);
+    delay(10);
+  }
+  for(int i = 255; i > -1;i--){
+    analogWrite(9,i);
+    analogWrite(10,i);
+    delay(10);
+  }
+}
