830aa8b227de7b276f6b5573f44cf8c60f3d645b
KIMDOKYOUNG <42798576+KIMDOKYOUNG@users.noreply.github.com>
2020-07-09 16:51:29 +0900

    Add files via upload

---
+++ lcd/lcd.ino
@@ -0,0 +1,27 @@
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(2,0);
+  lcd.print("STEVE & TARA"); // Hello, world!
+  lcd.setCursor(3,1);
+  lcd.print("2020/07/09");//Ywrobot Arduino!
+   lcd.setCursor(0,2);
+  lcd.print("Arduino LCM IIC 2004");
+   lcd.setCursor(2,3);
+  lcd.print("Power By Ec-yuan!");
+}
+
+
+void loop()
+{
+}
